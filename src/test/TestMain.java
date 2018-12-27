e1314b14df08ccf79d13d3a1bdd51e6b93f858c5
naokimura <naokimura@192.168.0.77>
2018-12-27 09:25:38 +0900

    TEST

---
+++ src/test/TestMain.java
@@ -0,0 +1,14 @@
+package test;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+import jp.go.nta.eTax.xml.xsd.general.Kingaku;
+
+
+
+public class TestMain {
+	
+	public static void main(String[] args) {
+	}
+}
